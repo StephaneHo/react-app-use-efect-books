0a6e295357374dfb56efbfba63c49a269604e11d
Stéphane Hordoir <stephane.hordoir@gmail.com>
2023-12-28 13:17:03 +0100

    first commit

---
+++ src/components/BookList.js
@@ -0,0 +1,18 @@
+import { BookDetails } from "./BookDetails";
+
+export const BookList = ({ books, onDelete, onEdit }) => {
+  const renderedBooks = books.map((book) => {
+    return (
+      <BookDetails
+        onEdit={onEdit}
+        onDelete={onDelete}
+        key={book.id}
+        book={book}
+      />
+    );
+  });
+
+  return <div className="book-list">{renderedBooks}</div>;
+};
+
+export default BookList;
